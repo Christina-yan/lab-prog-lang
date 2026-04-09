06a289fe2f87b5a9314df0881961132c6ee0083b
Christina-yan <yiaprosto@gmail.com>
2026-04-09 21:33:18 +0700

    lab 5 num 1 test

---
+++ Новая папка/Новая папка/contains.h
@@ -0,0 +1,22 @@
+//#define CONTAINS_H
+//#ifdef CONTAINS_H
+
+
+//template<typename T>
+//int index(T num, T* array, int size) {
+//    for (int i = 0; i < size; i++) {
+//        if (array[i] == num)
+//            return i;
+//    }
+//    return -1;
+//}
+//
+//template<typename T>
+//bool contains(T num, T* array, int size) {
+//    int num_index = num.index(num, array, size);
+//
+//    return num_index != -1 ? 1 : 0;
+//}
+
+//#endif // DEBUG
+
---
+++ Новая папка/Новая папка/task 1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+
+
+template<typename T>
+bool contains(T num, T* array, int size) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == num)
+            return 1;
+    }
+    return 0;
+}
+
+using namespace std;
+
+int main()
+{
+
+    int* a_array = new int[] {
+        1, 2, 3, 4, 5
+        };
+
+    int num;
+    cout << "enter int number" << endl;
+    cin >> num;
+
+    cout << contains(num, a_array, 5) << endl;
+
+    double* d_array = new double []{
+        1.5, 2.1, 3, 4.8, 5
+        };
+
+    string* s_array = new string[]{
+        "1", "2", "3", "4", "lalala"
+    };
+
+    delete[] a_array;
+
+    
+   
+
+
+
+
+}
