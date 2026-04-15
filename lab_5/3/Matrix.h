19fe7b05f713de1422d8d191fe306102f4461cc5
Christina-yan <yiaprosto@gmail.com>
2026-04-15 22:56:51 +0700

    lab 5 num 3

--- lab_5/3/Matrix.h
+++ lab_5/3/Matrix.h
@@ -1,81 +1,143 @@
+#ifndef MATRIX_H
 #define MATRIX_H
-#ifdef MATRIX_H
 
-# include <iostream>
+#include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 template<typename T>
 class Matrix {
 private:
-	vector<vector <T>> mtr;
+    vector<vector<T>> mtr;
 
-	void MakeRectangle() {
-		int max_size = 0;
-		for (const auto& row : mtr) {
-			if (row.size() > max_size) {
-				max_size = row.size();
-			}
-		}
-		for (auto& row : mtr) {
-			row.resize(max_size);
-		}
-	}
+    void MakeRectangle() {
+        int max_size = 0;
+        for (const auto& row : mtr) {
+            if (row.size() > max_size) {
+                max_size = row.size();
+            }
+        }
+        for (auto& row : mtr) {
+            row.resize(max_size);
+        }
+    }
+
 public:
-	Matrix(const vector<vector <T>>& m): mtr(m) {
-		MakeRectangle();
-	}
-	Matrix(int rows, int columns) {
-		mtr.resize(rows);
-		for (auto& row : mtr) {
-			row.resize(columns);
-		}
-	}
-	T Get(int i, int j) const {
-		return mtr[i][j];
-	}
-	int GetRows() {
-		return this->mtr.size();
-	}
-	int GetCols() {
-		return this->mtr[0].size();
-	}
-	void Set(int i, int j, T num) {
-		mtr[i][j] = num;
-	}
-	Matrix operator+ (const Matrix& m) const {
+    Matrix(const vector<vector<T>>& m) : mtr(m) {
+        MakeRectangle();
+    }
 
-		if (mtr.size() != m.mtr.size()) {
-			cout << "ERROR! matrix's size is not math" << endl;
-			return 0;
-			break;
-		}
-		
-		int rows = mtr.GetRows();
-		int cols = mtr.GetCols();
-		
-		Matrix res(rows, cols);
+    Matrix(int rows, int columns) {
+        mtr.resize(rows);
+        for (auto& row : mtr) {
+            row.resize(columns);
+        }
+    }
 
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				res.mtr[i][j] = this->mtr[i][j] + m.mtr[i][j];
-			}
-		}
-		return res;
-	};
+    Matrix() {}
 
-	friend ostream& operator << (ostream& out, const Matrix<T>& m) {
-		int rows = mtr.GetRows();
-		int cols = mtr.GetCols();
-		for (int i = 0; i != rows; i++) {
-			for (int j = 0; j != cols; j++) {
-				out << Matrix[i][j] << "\t";
-			}
-			out << "\n";
-		}
-		return out;
-	}
+    ~Matrix() {}
+
+    T Get(int i, int j) const {
+        return mtr[i][j];
+    }
+
+    void Set(int i, int j, T num) {
+        mtr[i][j] = num;
+    }
+
+    int GetRows() const {
+        return mtr.size();
+    }
+
+    int GetCols() const {
+        if (mtr.empty()) return 0;
+        return mtr[0].size();
+    }
+
+    Matrix operator+ (const Matrix& m) const {
+        if (mtr.size() != m.mtr.size() ||
+            (mtr.size() > 0 && mtr[0].size() != m.mtr[0].size())) {
+            cout << "ERROR! matrix's size is not match" << endl;
+            return Matrix();
+        }
+
+        int rows = GetRows();
+        int cols = GetCols();
+
+        Matrix res(rows, cols);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                res.mtr[i][j] = this->mtr[i][j] + m.mtr[i][j];
+            }
+        }
+        return res;
+    }
+
+    Matrix operator* (const Matrix& m) const {
+        if (GetCols() != m.GetRows()) {
+            cout << "ERROR! Cannot multiply matrices: columns of first != rows of second" << endl;
+            return Matrix();
+        }
+
+        int rows = GetRows();
+        int cols = m.GetCols();
+        int common = GetCols();
+
+        Matrix res(rows, cols);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                T sum = T();
+                for (int k = 0; k < common; k++) {
+                    sum = sum + (mtr[i][k] * m.mtr[k][j]);
+                }
+                res.mtr[i][j] = sum;
+            }
+        }
+        return res;
+    }
+
+    template<typename U>
+    friend ostream& operator<< (ostream& out, const Matrix<U>& m);
+
+    template<typename U>
+    friend istream& operator>> (istream& in, Matrix<U>& m);
 };
-#endif // 
 
+template<typename T>
+ostream& operator<< (ostream& out, const Matrix<T>& m) {
+    int rows = m.GetRows();
+    int cols = m.GetCols();
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            out << m.mtr[i][j] << "\t";
+        }
+        out << "\n";
+    }
+    return out;
+}
+
+template<typename T>
+istream& operator>> (istream& in, Matrix<T>& m) {
+    int rows, cols;
+    cout << "Enter number of rows: ";
+    in >> rows;
+    cout << "Enter number of columns: ";
+    in >> cols;
+
+    m.mtr.resize(rows);
+    for (int i = 0; i < rows; i++) {
+        m.mtr[i].resize(cols);
+        for (int j = 0; j < cols; j++) {
+            cout << "Enter element [" << i << "][" << j << "]: ";
+            in >> m.mtr[i][j];
+        }
+    }
+    return in;
+}
+
+#endif 
--- lab_5/3/task 3.cpp
+++ lab_5/3/task 3.cpp
@@ -1,16 +1,89 @@
 ﻿#include "Matrix.h"
+//#include <iostream>
+//#include <vector>
+//
 
-int main()
-{
-	int i = 2, j = 3;
-	Matrix<int> m({
-		{1,2,3},
-		{4,5,6},
-		{7,8,9}
-		});
+int main() {
+    
+    //int
+    Matrix<int> m1({
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+        });
 
-	//m.Set(0, 0, 5);
-	//cout << m.Get(0,0);
+    cout << "Matrix m1:" << endl;
+    cout << m1;
 
-	cout << m;
+    cout << "Item m1[0][0] = " << m1.Get(0, 0) << endl;
+    m1.Set(0, 0, 10);
+    cout << "after Set: m1[0][0] = " << m1.Get(0, 0) << endl;
+
+    Matrix<int> m2(3, 3);
+    m2.Set(0, 0, 1);
+    m2.Set(0, 1, 1);
+    m2.Set(0, 2, 1);
+    m2.Set(1, 0, 1);
+    m2.Set(1, 1, 1);
+    m2.Set(1, 2, 1);
+    m2.Set(2, 0, 1);
+    m2.Set(2, 1, 1);
+    m2.Set(2, 2, 1);
+
+    cout << "\nMatrix m2:" << endl;
+    cout << m2;
+
+    Matrix<int> m3 = m1 + m2;
+    cout << "m1 + m2:" << endl;
+    cout << m3;
+
+    Matrix<int> m4({
+        {1, 2},
+        {3, 4}
+        });
+
+    Matrix<int> m5({
+        {5, 6},
+        {7, 8}
+        });
+
+    cout << "\nMatrix m4:" << endl;
+    cout << m4;
+    cout << "Matrix m5:" << endl;
+    cout << m5;
+
+    Matrix<int> m6 = m4 * m5;
+    cout << "m4 * m5:" << endl;
+    cout << m6;
+
+    //string
+    Matrix<string> s1({
+        {"A", "B", "C"},
+        {"D", "E", "F"},
+        {"G", "H", "I"}
+        });
+
+    cout << "string matrix s1:" << endl;
+    cout << s1;
+
+    Matrix<string> s2(3, 3);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            s2.Set(i, j, "+");
+        }
+    }
+
+    cout << "string matrix s2:" << endl;
+    cout << s2;
+
+    Matrix<string> s3 = s1 + s2;
+    cout << "s1 + s2 :" << endl;
+    cout << s3;
+
+    Matrix<int> inputMatrix;
+    cin >> inputMatrix;
+    cout << "Matrix:" << endl;
+    cout << inputMatrix;
+
+    return 0;
 }
